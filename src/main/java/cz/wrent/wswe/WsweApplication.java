95e89718b3037e537b91b31967cbda69cf580e86
Adam Kucera <adam.kucera@wrent.cz>
2018-11-08 21:16:03 +0100

    Initial commit thanks to start.spring.io

---
+++ src/main/java/cz/wrent/wswe/WsweApplication.java
@@ -0,0 +1,12 @@
+package cz.wrent.wswe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsweApplication.class, args);
+	}
+}
